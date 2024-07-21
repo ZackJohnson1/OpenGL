f0bbe3d98e9c1eb26fc40cac7fa3b35f235cdeb3
zachjohnson <johnsza5@oregonstate.edu>
2024-07-21 17:57:34 -0600

    purple triangle

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@ const char *fragmentShaderSource = "#version 330 core\n"
     "out vec4 FragColor;\n"
     "void main()\n"
     "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "   FragColor = vec4(0.47f, 0.32f, 0.66f, 1.0f);\n" // Royal purple color
     "}\n\0";
 
 int main()
